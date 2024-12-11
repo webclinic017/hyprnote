51feef6c301db4bf517657e09be1301a92120af6
yujonglee <yujonglee.dev@gmail.com>
2024-12-11 11:24:11 +0900

    whisper using candle wip

--- src-tauri/src/stt.rs
+++ src-tauri/src/stt.rs
@@ -1,4 +1,42 @@
-use candle_transformers::models::whisper::{self as m, audio, Config};
+use cap_media::feeds::AudioInputSamplesReceiver;
+
+use candle_core::Device;
+use candle_nn::{ops::softmax, VarBuilder};
+use candle_transformers::models::whisper::{audio, model::Whisper, Config, DTYPE};
+
+pub struct STTState {
+    model: Whisper,
+    audio_receiver: AudioInputSamplesReceiver,
+}
+
+impl STTState {
+    pub async fn new(audio_rx: AudioInputSamplesReceiver) -> anyhow::Result<Self> {
+        // Initialize Whisper model
+        let device = Device::new_metal(0)?;
+
+        let config_filename = "whisper-tiny.en.bin";
+        let tokenizer_filename = "whisper-tiny.en.tokenizer.json";
+        let weights_filename = "whisper-tiny.en.bin";
+
+        let config: Config = serde_json::from_str(&std::fs::read_to_string(config_filename)?)?;
+
+        let vb =
+            unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DTYPE, &device)? };
+        let model = Whisper::load(&vb, config)?;
+
+        let tokenizer =
+            tokenizers::Tokenizer::from_file(tokenizer_filename).map_err(anyhow::Error::msg)?;
+
+        Ok(Self {
+            model,
+            audio_receiver: audio_rx,
+        })
+    }
+
+    pub async fn process_audio(&self) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
 
 // https://github.com/huggingface/candle/blob/main/candle-examples/examples/whisper/main.rs
 // https://github.com/huggingface/candle/blob/main/candle-examples/examples/whisper-microphone/main.rs
