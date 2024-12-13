280728c519eaca2771f051831ce30060983d454b
yujonglee <yujonglee.dev@gmail.com>
2024-12-13 14:26:03 +0900

    whipser stt implementation wip

--- crates/stt/src/lib.rs
+++ crates/stt/src/lib.rs
@@ -1,48 +1,44 @@
-use cap_media::feeds::AudioInputSamplesReceiver;
+use std::path::PathBuf;
 
 use candle_core::Device;
-use candle_nn::{ops::softmax, VarBuilder};
-use candle_transformers::models::whisper::{audio, model::Whisper, Config, DTYPE};
+use candle_nn::VarBuilder;
+use candle_transformers::models::whisper::{model::Whisper, Config, DTYPE};
 
 pub struct STTState {
     model: Whisper,
-    audio_receiver: AudioInputSamplesReceiver,
 }
+
+pub struct STTConfig {
+    whisper_config_path: PathBuf,
+    whisper_tokenizer_path: PathBuf,
+    whisper_weights_path: PathBuf,
+}
+
+mod whisper;
 
 // https://github.com/huggingface/candle/blob/main/candle-examples/examples/whisper/main.rs
 // https://github.com/huggingface/candle/blob/main/candle-examples/examples/whisper-microphone/main.rs
 impl STTState {
-    pub async fn new(audio_rx: AudioInputSamplesReceiver) -> anyhow::Result<Self> {
-        // Initialize Whisper model
+    pub async fn new(stt_config: STTConfig) -> anyhow::Result<Self> {
         let device = Device::new_metal(0)?;
 
-        let config_filename = "whisper-tiny.en.bin";
-        let tokenizer_filename = "whisper-tiny.en.tokenizer.json";
-        let weights_filename = "whisper-tiny.en.bin";
+        let config: Config =
+            serde_json::from_str(&std::fs::read_to_string(stt_config.whisper_config_path)?)?;
 
-        let config: Config = serde_json::from_str(&std::fs::read_to_string(config_filename)?)?;
-
-        let vb =
-            unsafe { VarBuilder::from_mmaped_safetensors(&[weights_filename], DTYPE, &device)? };
+        let vb = unsafe {
+            VarBuilder::from_mmaped_safetensors(&[stt_config.whisper_weights_path], DTYPE, &device)?
+        };
         let model = Whisper::load(&vb, config)?;
 
-        let tokenizer =
-            tokenizers::Tokenizer::from_file(tokenizer_filename).map_err(anyhow::Error::msg)?;
+        let _ = tokenizers::Tokenizer::from_file(stt_config.whisper_tokenizer_path)
+            .map_err(anyhow::Error::msg)?;
 
-        Ok(Self {
-            model,
-            audio_receiver: audio_rx,
-        })
-    }
-
-    pub async fn process_audio(&self) -> anyhow::Result<()> {
-        Ok(())
+        Ok(Self { model })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use tokio;
 
     #[tokio::test]
---
+++ crates/stt/src/whisper.rs
@@ -0,0 +1,3 @@
+struct Decoder {}
+
+impl Decoder {}
