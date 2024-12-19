251b9b707167017b21be0f59cbcedf20889b6869
yujonglee <yujonglee.dev@gmail.com>
2024-12-19 12:31:50 +0900

    remove landing page

--- apps/web/src/data/logoData.ts
+++
@@ -1,8 +0,0 @@
-export const universities = [
-  { title: "UC Berkeley", file: "BerkeleyIcon" },
-  { title: "Stanford", file: "StanfordIcon" },
-  { title: "UC Berkeley", file: "BerkeleyIcon" },
-  { title: "UC Berkeley", file: "BerkeleyIcon" },
-  { title: "UC Berkeley", file: "BerkeleyIcon" },
-  { title: "UC Berkeley", file: "BerkeleyIcon" },
-];
