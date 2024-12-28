b3e243aa44b1c20a8bac1e5ff7ce288d7bd7e7dc
yujonglee <yujonglee.dev@gmail.com>
2024-12-28 20:41:12 +0900

    add host crate

---
+++ crates/host/src/lib.rs
@@ -0,0 +1,64 @@
+use mac_address::get_mac_address;
+use std::hash::{DefaultHasher, Hash, Hasher};
+use sysinfo::System;
+
+pub fn cpu_arch() -> String {
+    System::cpu_arch()
+}
+
+pub fn long_os_version() -> String {
+    System::long_os_version().unwrap_or("Unknown".to_string())
+}
+
+pub fn fingerprint() -> String {
+    let mac_address = get_mac_address()
+        .ok()
+        .flatten()
+        .map(|addr| addr.to_string())
+        .unwrap_or_default();
+
+    let sys_name = System::name().unwrap_or_default();
+    let sys_host_name = System::host_name().unwrap_or_default();
+    let sys_cpu_arch = System::cpu_arch();
+
+    let fingerprint = format!(
+        "{}-{}-{}-{}",
+        sys_cpu_arch, sys_name, sys_host_name, mac_address
+    );
+
+    let mut hasher = DefaultHasher::new();
+    fingerprint.hash(&mut hasher);
+    hasher.finish().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_long_os_version() {
+        let a = long_os_version();
+        let b = long_os_version();
+        let c = long_os_version();
+        assert_eq!(a, b);
+        assert_eq!(a, c);
+    }
+
+    #[test]
+    fn test_cpu_arch() {
+        let a = cpu_arch();
+        let b = cpu_arch();
+        let c = cpu_arch();
+        assert_eq!(a, b);
+        assert_eq!(a, c);
+    }
+
+    #[test]
+    fn test_fingerprint() {
+        let a = fingerprint();
+        let b = fingerprint();
+        let c = fingerprint();
+        assert_eq!(a, b);
+        assert_eq!(a, c);
+    }
+}
