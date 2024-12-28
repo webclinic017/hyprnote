a30109358ace873ccb0f99c6a9ce334cf329969a
yujonglee <yujonglee.dev@gmail.com>
2024-12-29 07:25:39 +0900

    update deps

--- crates/audio/src/speaker/macos.rs
+++ crates/audio/src/speaker/macos.rs
@@ -4,7 +4,7 @@ use futures_util::StreamExt;
 use ca::aggregate_device_keys as agg_keys;
 use cidre::{arc, av, cat, cf, core_audio as ca, ns, os};
 
-// https://github.com/yury/cidre/blob/23efaabee6bf75bfb57a9e7739b2beb83cb93942/cidre/examples/core-audio-record/main.rs
+// https://github.com/yury/cidre/blob/7bc6c3addbd3fe2c586fbe15319a17f6d16c9049/cidre/examples/core-audio-record/main.rs
 // https://github.com/floneum/floneum/blob/92129ec99aac446348f42bc6db326a6d1c2d99ae/interfaces/kalosm-sound/src/source/mic.rs#L41
 #[cfg(target_os = "macos")]
 pub struct SpeakerInput {
