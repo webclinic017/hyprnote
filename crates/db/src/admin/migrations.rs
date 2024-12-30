4001be9ba93da0108e03a94fc738c30875a3eb03
yujonglee <yujonglee.dev@gmail.com>
2024-12-30 14:58:32 +0900

    find user from api_key in device table

--- crates/db/src/admin/migrations.rs
+++ crates/db/src/admin/migrations.rs
@@ -17,5 +17,6 @@ pub fn v0() -> Vec<impl AsRef<str>> {
             FOREIGN KEY (user_id) REFERENCES users(id)
         );",
         "CREATE INDEX idx_devices_api_key ON devices(api_key);",
+        "CREATE UNIQUE INDEX IF NOT EXISTS devices_user_id_fingerprint ON devices(user_id, fingerprint);",
     ]
 }
--- crates/db/src/admin/ops.rs
+++ crates/db/src/admin/ops.rs
@@ -1,5 +1,6 @@
 use super::{Device, User};
 use anyhow::Result;
+use rand::{distributions::Alphanumeric, Rng};
 
 pub struct AdminDatabase {
     conn: libsql::Connection,
@@ -9,19 +10,9 @@ impl AdminDatabase {
     pub async fn from(conn: libsql::Connection) -> Self {
         Self { conn }
     }
+}
 
-    pub async fn list_devices(&self) -> Result<Vec<Device>> {
-        let mut rows = self.conn.query("SELECT * FROM devices", ()).await.unwrap();
-        let mut devices = Vec::new();
-
-        while let Some(row) = rows.next().await.unwrap() {
-            let device: Device = libsql::de::from_row(&row).unwrap();
-            devices.push(device);
-        }
-
-        Ok(devices)
-    }
-
+impl AdminDatabase {
     pub async fn list_users(&self) -> Result<Vec<User>> {
         let mut rows = self.conn.query("SELECT * FROM users", ()).await.unwrap();
         let mut users = Vec::new();
@@ -52,6 +43,49 @@ impl AdminDatabase {
         Ok(user)
     }
 
+    pub async fn list_devices(&self) -> Result<Vec<Device>> {
+        let mut rows = self.conn.query("SELECT * FROM devices", ()).await.unwrap();
+        let mut devices = Vec::new();
+
+        while let Some(row) = rows.next().await.unwrap() {
+            let device: Device = libsql::de::from_row(&row).unwrap();
+            devices.push(device);
+        }
+
+        Ok(devices)
+    }
+    pub async fn upsert_device(&self, device: Device) -> Result<Device> {
+        let mut rows = self
+            .conn
+            .query(
+                "INSERT INTO devices (
+                    user_id,
+                    fingerprint,
+                    api_key
+                ) VALUES (?, ?, ?)
+                ON CONFLICT (user_id, fingerprint) DO UPDATE SET
+                    api_key = excluded.api_key
+                RETURNING *",
+                vec![
+                    device.user_id.to_string(),
+                    device.fingerprint,
+                    generate_api_key(),
+                ],
+            )
+            .await?;
+
+        let row = rows.next().await.unwrap().unwrap();
+        let device: Device = libsql::de::from_row(&row).unwrap();
+        Ok(device)
+    }
+
+    pub async fn delete_device_with_api_key(&self, api_key: String) -> Result<()> {
+        self.conn
+            .query("DELETE FROM devices WHERE api_key = ?", vec![api_key])
+            .await?;
+        Ok(())
+    }
+
     pub async fn get_user_by_clerk_user_id(&self, clerk_user_id: String) -> Result<User> {
         let mut rows = self
             .conn
@@ -66,28 +100,31 @@ impl AdminDatabase {
         Ok(user)
     }
 
-    pub async fn create_device(&self, device: Device) -> Result<Device> {
+    pub async fn get_user_by_device_api_key(&self, api_key: String) -> Result<User> {
         let mut rows = self
             .conn
             .query(
-                "INSERT INTO devices (
-                    user_id,
-                    fingerprint,
-                    api_key
-                ) VALUES (?, ?, ?) 
-                RETURNING *",
-                vec![
-                    device.user_id.to_string(),
-                    device.fingerprint,
-                    device.api_key,
-                ],
+                "SELECT users.* FROM users 
+                JOIN devices ON devices.user_id = users.id 
+                WHERE devices.api_key = ?",
+                vec![api_key],
             )
             .await?;
 
         let row = rows.next().await.unwrap().unwrap();
-        let device: Device = libsql::de::from_row(&row).unwrap();
-        Ok(device)
+        let user: User = libsql::de::from_row(&row).unwrap();
+        Ok(user)
     }
+}
+
+fn generate_api_key() -> String {
+    let key: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(32)
+        .map(char::from)
+        .collect();
+
+    format!("hypr_{}", key)
 }
 
 #[cfg(test)]
@@ -145,14 +182,43 @@ mod tests {
             .unwrap();
 
         let device = db
-            .create_device(Device {
+            .upsert_device(Device {
                 user_id: user.id,
                 fingerprint: "3".to_string(),
-                api_key: "4".to_string(),
                 ..Device::default()
             })
             .await
             .unwrap();
         assert_eq!(device.user_id, user.id);
+    }
+
+    #[tokio::test]
+    async fn test_get_user_by_device_api_key() {
+        let db = setup_db().await;
+
+        let user_1 = db
+        .create_user(User {
+            clerk_user_id: "21".to_string(),
+            turso_db_name: "12".to_string(),
+            ..User::default()
+        })
+        .await
+        .unwrap();
+
+        let device = db
+            .upsert_device(Device {
+                user_id: user_1.id,
+                fingerprint: "3".to_string(),
+                ..Device::default()
+            })
+            .await
+            .unwrap();
+
+        let user_2 = db
+            .get_user_by_device_api_key(device.api_key)
+            .await
+            .unwrap();
+
+        assert_eq!(user_1.id, user_2.id);
     }
 }
