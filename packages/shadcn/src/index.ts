3273000070d856ccf77b99f8cffd8300579a4c02
yujonglee <yujonglee.dev@gmail.com>
2025-01-04 14:44:58 +0900

    chores

--- packages/shadcn/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "@/components/ui/button";
-export * from "@/components/ui/dialog";
-export * from "@/components/ui/command";
-export * from "@/components/ui/select";
-export * from "@/components/ui/carousel";
