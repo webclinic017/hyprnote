4e95c985e02093ddcda7f1bb458fc7047b939495
yujonglee <yujonglee.dev@gmail.com>
2025-01-23 12:23:46 +0900

    update onnx config

--- crates/onnx/src/lib.rs
+++ crates/onnx/src/lib.rs
@@ -1,11 +1,17 @@
 #[allow(unused_imports)]
-use ort::execution_providers::{CoreMLExecutionProvider, DirectMLExecutionProvider};
-use ort::{error::Result, session::Session};
+use ort::{
+    error::Result,
+    execution_providers::{CoreMLExecutionProvider, DirectMLExecutionProvider},
+    session::{builder::GraphOptimizationLevel, Session},
+};
 
 pub use ort;
 
 pub fn load_model(bytes: &[u8]) -> Result<Session> {
     let session = Session::builder()?
+        .with_intra_threads(1)?
+        .with_inter_threads(1)?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
         .with_execution_providers([
             #[cfg(target_os = "windows")]
             DirectMLExecutionProvider::default().build(),
