e3fb06c432bde7806441fafe60cba829b243fb9b
yujonglee <yujonglee.dev@gmail.com>
2025-01-23 12:37:15 +0900

    remove execution provider

--- crates/onnx/src/lib.rs
+++ crates/onnx/src/lib.rs
@@ -1,7 +1,5 @@
-#[allow(unused_imports)]
 use ort::{
     error::Result,
-    execution_providers::{CoreMLExecutionProvider, DirectMLExecutionProvider},
     session::{builder::GraphOptimizationLevel, Session},
 };
 
@@ -12,12 +10,6 @@ pub fn load_model(bytes: &[u8]) -> Result<Session> {
         .with_intra_threads(1)?
         .with_inter_threads(1)?
         .with_optimization_level(GraphOptimizationLevel::Level3)?
-        .with_execution_providers([
-            #[cfg(target_os = "windows")]
-            DirectMLExecutionProvider::default().build(),
-            #[cfg(target_os = "macos")]
-            CoreMLExecutionProvider::default().build(),
-        ])?
         .commit_from_memory(bytes)?;
 
     Ok(session)
