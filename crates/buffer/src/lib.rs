893a39fb2f0bc4163e1e0a0bbddf6c59ff8200ca
yujonglee <yujonglee.dev@gmail.com>
2025-01-25 16:02:28 +0900

    update q tag in buffer

--- crates/buffer/src/lib.rs
+++ crates/buffer/src/lib.rs
@@ -6,6 +6,10 @@ pub enum Error {
     MarkdownParseError(String),
     #[error("failed to render markdown")]
     MarkdownRenderError(String),
+    #[error("failed to render html")]
+    HTMLRenderError(String),
+    #[error("failed to parse html")]
+    HTMLParseError(String),
 }
 
 pub struct Buffer {
@@ -26,12 +30,13 @@ impl Buffer {
 
     pub fn read(&self) -> Result<String, Error> {
         let content = self.content.lock().unwrap();
-        md_to_html(&content)
+        let md = md_to_md(&content)?;
+        md_to_html(&md)
     }
 }
 
-fn md_to_html(md: &str) -> Result<String, Error> {
-    let mut ast = markdown::to_mdast(md, &markdown::ParseOptions::default())
+fn md_to_md(text: &str) -> Result<String, Error> {
+    let mut ast = markdown::to_mdast(text, &markdown::ParseOptions::default())
         .map_err(|e| Error::MarkdownParseError(e.to_string()))?;
 
     convert_ordered_to_unordered(&mut ast);
@@ -43,8 +48,26 @@ fn md_to_html(md: &str) -> Result<String, Error> {
     )
     .map_err(|e| Error::MarkdownRenderError(e.to_string()))?;
 
-    let html = markdown::to_html(&md);
-    Ok(html)
+    Ok(md)
+}
+
+fn md_to_html(text: &str) -> Result<String, Error> {
+    let html = markdown::to_html_with_options(
+        &text,
+        &markdown::Options {
+            parse: markdown::ParseOptions::default(),
+            compile: markdown::CompileOptions {
+                allow_dangerous_html: true,
+                ..Default::default()
+            },
+        },
+    )
+    .map_err(|e| Error::HTMLRenderError(e.to_string()))?;
+
+    let dom = tl::parse(&html, tl::ParserOptions::default())
+        .map_err(|e| Error::HTMLParseError(e.to_string()))?;
+
+    Ok(dom.outer_html())
 }
 
 fn convert_ordered_to_unordered(node: &mut markdown::mdast::Node) {
@@ -125,5 +148,28 @@ mod tests {
             result,
             "<p>hi</p>\n<h2>World</h2>\n<ul>\n<li>Hi</li>\n<li>Bye!</li>\n</ul>\n<h3>World</h3>\n<h3>World</h3>\n<h3>World</h3>\n"
         );
+    }
+
+    #[test]
+    fn test_3() {
+        let buffer = Buffer::new();
+
+        buffer.write("hi\n");
+        buffer.write("<q>quote</q>\n\n");
+        buffer.write("bye");
+
+        let result = buffer.read().unwrap();
+        assert_eq!(result, "<p>hi <q>quote </q></p>\n<p>bye</p>\n");
+    }
+
+    #[test]
+    fn test_4() {
+        let buffer = Buffer::new();
+
+        buffer.write("hi\n");
+        buffer.write("<q>quote");
+
+        let result = buffer.read().unwrap();
+        assert_eq!(result, "<p>hi <q>quote\n</q></p>");
     }
 }
