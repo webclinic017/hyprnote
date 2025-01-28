aafdda025454a3ae7ad7c92a8423d00a68c283ba
yujonglee <yujonglee.dev@gmail.com>
2025-01-28 10:24:52 +0900

    new session implementation

--- apps/desktop/src-tauri/src/session.rs
+++ apps/desktop/src-tauri/src/session.rs
@@ -19,7 +19,7 @@ impl SessionState {
         &mut self,
         app_dir: std::path::PathBuf,
         session_id: String,
-        _channel: tauri::ipc::Channel<Vec<f32>>,
+        channel: tauri::ipc::Channel<Vec<f32>>,
     ) -> anyhow::Result<()> {
         let mic_stream = {
             let source = hypr_audio::MicInput::default();
@@ -29,8 +29,8 @@ impl SessionState {
         .chunks(1024);
 
         let speaker_stream = {
-            let source = hypr_audio::SpeakerInput::new().unwrap();
-            source.stream().unwrap()
+            let source = hypr_audio::SpeakerInput::new()?;
+            source.stream()?
         }
         .resample(16000)
         .chunks(1024);
@@ -44,52 +44,57 @@ impl SessionState {
             bits_per_sample: 16,
             sample_format: hound::SampleFormat::Int,
         };
-
-        // let transcribe_client = hypr_bridge::Client::builder()
-        //     .api_base("http://localhost:8000")
-        //     .api_key("TODO")
-        //     .build()
-        //     .unwrap()
-        //     .transcribe()
-        //     .language(codes_iso_639::part_1::LanguageCode::En)
-        //     .build();
-
         let path = app_dir.join(format!("{}.wav", session_id));
-        // let (_tx, rx) = tokio::sync::mpsc::channel(32);
 
-        // let _transcribe_handle = {
-        //     tokio::spawn(async move {
-        //         let input_stream = hypr_audio::ReceiverStreamSource::new(rx, 16000);
-        //         let transcript_stream = transcribe_client.from_audio(input_stream).await.unwrap();
-        //         futures_util::pin_mut!(transcript_stream);
+        let (ws_tx, ws_rx) = tokio::sync::mpsc::channel(32);
 
-        //         while let Some(transcript) = transcript_stream.next().await {
-        //             println!("Transcript: {:?}", transcript);
-        //         }
-        //     })
-        // };
+        let _transcript_handle = {
+            tokio::spawn(async move {
+                let ws_stream = hypr_audio::ReceiverStreamSource::new(ws_rx, 16000);
+                let client = hypr_bridge::Client::builder()
+                    .api_base("http://localhost:8000")
+                    .api_key("your_api_key")
+                    .build()?
+                    .transcribe()
+                    .language(codes_iso_639::part_1::LanguageCode::En)
+                    .build();
 
-        let handle: tauri::async_runtime::JoinHandle<()> =
-            tauri::async_runtime::spawn(async move {
-                let mut writer = hound::WavWriter::create(&path, spec).unwrap();
+                let transcript_stream = client.from_audio(ws_stream).await?;
+                futures_util::pin_mut!(transcript_stream);
 
-                let mut combined_stream = mic_stream.zip(speaker_stream);
+                while let Some(transcript) = transcript_stream.next().await {
+                    println!("Transcript: {:?}", transcript);
+                    channel.send(vec![])?;
+                }
+                anyhow::Ok(())
+            })
+        };
 
-                // TODO: we're not receiving any mic stream after removing kalosm.
-                tokio::select! {
-                    _ = shutdown_rx => {}
-                    _ = async {
-                        while let Some((mic_chunk, speaker_chunk)) = combined_stream.next().await {
-                            let mixed = hypr_audio::mix(&mic_chunk, &speaker_chunk);
+        let handle = tauri::async_runtime::spawn(async move {
+            let mut writer = hound::WavWriter::create(&path, spec).unwrap();
+            let mut combined_stream = mic_stream.zip(speaker_stream);
 
-                            for sample in mixed {
-                                writer.write_sample(sample.to_sample::<i16>()).unwrap();
+            tokio::select! {
+                _ = shutdown_rx => {}
+                _ = async {
+                    while let Some((mic_chunk, speaker_chunk)) = combined_stream.next().await {
+                        let mixed = hypr_audio::mix(&mic_chunk, &speaker_chunk);
+
+                        for sample in &mixed {
+                            writer.write_sample(sample.to_sample::<i16>()).unwrap();
+                        }
+
+                        for sample in mixed {
+                            if let Err(_) = ws_tx.send(sample).await {
+                                break;
                             }
                         }
-                        let _ = writer.finalize();
-                    } => {},
-                }
-            });
+                    }
+                    let _ = writer.finalize();
+                    anyhow::Ok(())
+                } => {},
+            }
+        });
 
         self.handle = Some(handle);
         Ok(())
@@ -105,10 +110,6 @@ impl SessionState {
         if let Some(handle) = self.handle.take() {
             handle.abort();
         }
-
-        // TODO:
-        // upload... but we can not call stt direclty here.
-        // need to call use bridge to upload chu
 
         self.handle = None;
         self.shutdown = None;
