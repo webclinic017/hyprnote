92569e750486caf964b33b1e99ba77df995adb71
yujonglee <yujonglee.dev@gmail.com>
2025-02-12 17:02:19 +0900

    missing implementation for speaker stream in windows

--- crates/audio/src/speaker/windows.rs
+++ crates/audio/src/speaker/windows.rs
@@ -1,5 +1,15 @@
 pub struct SpeakerInput {}
 
+impl SpeakerInput {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn stream(self) -> SpeakerStream {
+        SpeakerStream::new()
+    }
+}
+
 pub struct SpeakerStream {}
 
 impl SpeakerStream {
@@ -9,5 +19,16 @@ impl SpeakerStream {
 
     pub fn sample_rate(&self) -> u32 {
         16000
+    }
+}
+
+impl Stream for SpeakerStream {
+    type Item = f32;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        std::task::Poll::Ready(Some(0.0))
     }
 }
