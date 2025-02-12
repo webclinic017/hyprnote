77a90c49c2cd1b80747ae21675f36c56eff8ec8b
yujonglee <yujonglee.dev@gmail.com>
2025-02-12 14:24:23 +0900

    deploy docs to cloudflare

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,28 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import db from "@astrojs/db";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [
+    db(),
+    starlight({
+      title: "Hyprnote Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", slug: "guides/example" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
