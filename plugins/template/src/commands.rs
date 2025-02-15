656d181dd80a60bb4fd8d7ae2ba041d16f807623
yujonglee <yujonglee.dev@gmail.com>
2025-02-15 12:48:06 +0900

    chores

--- plugins/template/src/commands.rs
+++ plugins/template/src/commands.rs
@@ -1,3 +1,5 @@
+use codes_iso_639::part_1::LanguageCode;
+
 #[tauri::command]
 #[specta::specta]
 pub async fn render(tpl: String, ctx: String) -> Result<String, String> {
@@ -5,7 +7,18 @@ pub async fn render(tpl: String, ctx: String) -> Result<String, String> {
     let context = tera::Context::from_serialize(ctx).map_err(|e| e.to_string())?;
 
     let mut tera = tera::Tera::default();
+
     tera.register_filter("language", filters::language());
+
+    [LanguageCode::En, LanguageCode::Ko]
+        .iter()
+        .for_each(|lang| {
+            tera.register_tester(
+                lang.language_name().to_lowercase().as_str(),
+                testers::language(*lang),
+            );
+        });
+
     tera.autoescape_on(vec![]);
 
     tera.render_str(&tpl, &context).map_err(|e| e.to_string())
@@ -28,6 +41,31 @@ mod filters {
                     let lang_name = lang_code.unwrap().language_name();
                     Ok(Value::String(lang_name.to_string()))
                 }
+            },
+        )
+    }
+}
+
+mod testers {
+    use codes_iso_639::part_1::LanguageCode;
+    use serde_json::Value;
+
+    pub fn language(lang: LanguageCode) -> impl tera::Test {
+        Box::new(
+            move |value: Option<&Value>, _args: &[Value]| -> tera::Result<bool> {
+                if value.is_none() {
+                    return Err(tera::Error::msg("'value' is empty"));
+                }
+
+                let maybe_lhs = value.unwrap().as_str();
+                if maybe_lhs.is_none() {
+                    return Err(tera::Error::msg("'value' is not a string"));
+                }
+
+                let lhs = maybe_lhs.unwrap().to_lowercase();
+                let rhs = lang.code().to_lowercase();
+
+                Ok(lhs == rhs)
             },
         )
     }
