cc3bb3bcb1c4dc78cac1832ea586d94f8a67a01b
yujonglee <yujonglee.dev@gmail.com>
2025-02-15 15:38:15 +0900

    generate config schema from rust struct

--- crates/db/src/user/extensions_types.rs
+++ crates/db/src/user/extensions_types.rs
@@ -3,6 +3,7 @@ use crate::user_common_derives;
 user_common_derives! {
     pub struct ExtensionDefinition {
         pub id: String,
+        pub default_enabled: bool,
         pub name: String,
         pub description: String,
         pub config_schema: String,
@@ -16,5 +17,22 @@ user_common_derives! {
         pub extension_id: String,
         pub enabled: bool,
         pub config: serde_json::Value,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extension_definition() {
+        let generator = schemars::gen::SchemaSettings::draft07().into_generator();
+        let schema = generator.into_root_schema_for::<ExtensionDefinition>();
+
+        let current_dir = env!("CARGO_MANIFEST_DIR");
+        let config_schema_path =
+            std::path::PathBuf::from(current_dir).join("../../extensions/config.schema.json");
+        let config_schema_content = serde_json::to_string_pretty(&schema).unwrap();
+        std::fs::write(config_schema_path, config_schema_content).unwrap();
     }
 }
