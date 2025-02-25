e2a94a3a3963080a688647ef8e58d8b5a8439ce3
yujonglee <yujonglee.dev@gmail.com>
2025-02-25 19:45:38 +0900

    remove example extension

--- extensions/example/src/init.ts
+++
@@ -1,7 +0,0 @@
-import { Extension } from "../../types";
-
-const init: Extension["init"] = async () => {
-  console.log("Example extension initialized");
-};
-
-export default init;
--- extensions/example/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import type { Config } from "tailwindcss";
-
-const config = {
-  content: [path.resolve(__dirname, "./src/*.tsx")],
-} satisfies Config;
-
-export default config;
