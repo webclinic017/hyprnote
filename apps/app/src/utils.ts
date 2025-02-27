d922d22e20f61e08449779d8fe70744966475a4e
yujonglee <yujonglee.dev@gmail.com>
2025-02-27 16:23:28 +0900

    fix createURL utils

--- apps/app/src/utils.ts
+++ apps/app/src/utils.ts
@@ -1,9 +1,12 @@
 import { LinkProps } from "@tanstack/react-router";
 
 export const createURL = (to: LinkProps["to"], search: LinkProps["search"]) => {
-  const url = new URL(to as string);
+  const url = new URL(window.location.origin);
+  url.pathname = to as string;
+
   const params = new URLSearchParams(search as Record<string, string>);
   url.search = params.toString();
+
   return url;
 };
 
