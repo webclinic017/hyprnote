8eb795366fdb5acd3adb1581164812021e3a1a60
yujonglee <yujonglee.dev@gmail.com>
2025-03-09 11:17:59 +0900

    refactor tray plugin

--- plugins/tray/src/lib.rs
+++ plugins/tray/src/lib.rs
@@ -1,8 +1,64 @@
+use std::sync::Mutex;
+use tauri::Manager;
+
 mod ext;
 pub use ext::*;
 
-pub fn init() -> tauri::plugin::TauriPlugin<tauri::Wry> {
-    tauri::plugin::Builder::new("tray")
-        .invoke_handler(tauri::generate_handler![])
+pub type SharedState = Mutex<State>;
+
+#[derive(Default)]
+pub struct State {
+    pub always_on_top: Option<bool>,
+}
+
+const PLUGIN_NAME: &str = "tray";
+
+fn make_specta_builder<R: tauri::Runtime>() -> tauri_specta::Builder<R> {
+    tauri_specta::Builder::<R>::new()
+        .plugin_name(PLUGIN_NAME)
+        .commands(tauri_specta::collect_commands![])
+        .error_handling(tauri_specta::ErrorHandlingMode::Throw)
+}
+
+pub fn init<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
+    let specta_builder = make_specta_builder();
+
+    tauri::plugin::Builder::new(PLUGIN_NAME)
+        .invoke_handler(specta_builder.invoke_handler())
+        .setup(|app, _api| {
+            let state = SharedState::default();
+            assert!(app.manage(state));
+            Ok(())
+        })
         .build()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn export_types() {
+        make_specta_builder::<tauri::Wry>()
+            .export(
+                specta_typescript::Typescript::default()
+                    .header("// @ts-nocheck\n\n")
+                    .formatter(specta_typescript::formatter::prettier)
+                    .bigint(specta_typescript::BigIntExportBehavior::Number),
+                "./js/bindings.gen.ts",
+            )
+            .unwrap()
+    }
+
+    fn create_app<R: tauri::Runtime>(builder: tauri::Builder<R>) -> tauri::App<R> {
+        builder
+            .plugin(init())
+            .build(tauri::test::mock_context(tauri::test::noop_assets()))
+            .unwrap()
+    }
+
+    #[test]
+    fn test_template() {
+        let _app = create_app(tauri::test::mock_builder());
+    }
 }
