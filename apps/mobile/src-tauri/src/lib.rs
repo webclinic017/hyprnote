8520131bce7518d69295d8138c6691fa6fe56f6f
yujonglee <yujonglee.dev@gmail.com>
2025-03-12 20:06:29 +0900

    add fs plugin

--- apps/mobile/src-tauri/src/lib.rs
+++ apps/mobile/src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ pub fn run() {
 
     let builder = tauri::Builder::default()
         .plugin(tauri_plugin_auth::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_misc::init())
