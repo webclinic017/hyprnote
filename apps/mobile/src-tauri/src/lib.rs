9acf916b89b27b5d4049c2352a2311dcdc8aade8
yujonglee <yujonglee.dev@gmail.com>
2025-03-13 09:48:49 +0900

    add notification plugin in mobile

--- apps/mobile/src-tauri/src/lib.rs
+++ apps/mobile/src-tauri/src/lib.rs
@@ -18,6 +18,7 @@ pub fn run() {
     let builder = tauri::Builder::default()
         .plugin(tauri_plugin_auth::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_misc::init())
