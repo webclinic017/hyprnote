93e788cdacb4e131a68b6756afe8b85ed797659c
yujonglee <yujonglee.dev@gmail.com>
2025-03-17 19:19:48 +0900

    add loops and nango to restate handler

--- apps/restate/src/checkout_service.rs
+++ apps/restate/src/checkout_service.rs
@@ -20,12 +20,17 @@ pub struct CheckoutServiceImpl;
 impl CheckoutService for CheckoutServiceImpl {
     async fn handle(
         &self,
-        _ctx: Context<'_>,
+        ctx: Context<'_>,
         Json(CheckoutRequest {
             user_id: _,
             tickets: _,
         }): Json<CheckoutRequest>,
     ) -> Result<bool, HandlerError> {
+        let loops = hypr_loops::LoopClientBuilder::default()
+            .api_key("TODO")
+            .build();
+
+        ctx.run(|| send_checkout_event(&loops)).await?;
         Ok(true)
     }
 
@@ -33,4 +38,17 @@ impl CheckoutService for CheckoutServiceImpl {
     async fn on_stripe_webhook(&self, _ctx: Context<'_>) -> Result<bool, HandlerError> {
         Ok(true)
     }
+}
+
+async fn send_checkout_event(
+    loops: &hypr_loops::LoopClient,
+) -> Result<Json<hypr_loops::Response>, HandlerError> {
+    let res = loops
+        .send_event(hypr_loops::Event {
+            name: "checkout".to_string(),
+            properties: None,
+        })
+        .await?;
+
+    Ok(Json(res))
 }
