0864779c4cf4d6e9b36018516f7351137dcfdfa5
yujonglee <yujonglee.dev@gmail.com>
2025-03-23 14:24:20 +0900

    fix issues around model downloading

--- apps/desktop/src/components/toast/model.tsx
+++ apps/desktop/src/components/toast/model.tsx
@@ -21,6 +21,10 @@ export default function ModelDownloadNotification() {
   });
 
   useEffect(() => {
+    if (!checkForModelDownload.data) {
+      return;
+    }
+
     if (checkForModelDownload.data?.stt && checkForModelDownload.data?.llm) {
       return;
     }
