fa5f78127eba5510953db32b15837d3b761f12ab
yujonglee <yujonglee.dev@gmail.com>
2025-04-11 10:44:05 +0900

    prevent starting list right after header

--- crates/llama/src/grammar/mod.rs
+++ crates/llama/src/grammar/mod.rs
@@ -19,21 +19,42 @@ mod tests {
     fn test_markdown_grammar() {
         let test_cases = vec![
             (
-                "freeCodeCamp",
-                "root ::= .*",
-                include_url!("https://raw.githubusercontent.com/freeCodeCamp/freeCodeCamp/refs/heads/main/README.md"),
-            ),
-            (
-                "build-your-own-x",
                 "root ::= .*",
                 include_url!("https://raw.githubusercontent.com/codecrafters-io/build-your-own-x/refs/heads/master/README.md"),
+                true
+            ),
+            (
+                MARKDOWN_GRAMMAR,
+                indoc::indoc! {r#"
+                Here's a response:
+
+                # This is a test"#},
+                false,
+            ),
+            (
+                MARKDOWN_GRAMMAR,
+                indoc::indoc! {r#"
+                # This is a test
+
+                - First
+                - Second
+                - Third"#},
+                false,
+            ),
+            (
+                MARKDOWN_GRAMMAR,
+                indoc::indoc! {r#"
+                # Hello World
+                ## This is a test
+
+                Test"#},
+                true,
             ),
         ];
 
-        for (name, grammar, content) in test_cases {
-            let transformed_md = hypr_buffer::opinionated_md_to_md(content).unwrap();
-            let validated = gbnf_validator::llama_gbnf_validator(grammar, &transformed_md).unwrap();
-            assert!(validated, "Validation failed for: {}", name);
+        for (i, (grammar, text, expected)) in test_cases.iter().enumerate() {
+            let validated = gbnf_validator::llama_gbnf_validator(grammar, text).unwrap();
+            assert_eq!(validated, *expected, "{}_th_test_case_failed", i);
         }
     }
 }
