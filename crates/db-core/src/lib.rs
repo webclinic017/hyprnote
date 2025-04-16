316dd6acba518297cbef1505eef8765644b1ca48
yujonglee <yujonglee.dev@gmail.com>
2025-04-16 13:25:56 +0900

    fix db connection management for local-only ops

--- crates/db-core/src/lib.rs
+++ crates/db-core/src/lib.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 mod errors;
 pub use errors::*;
@@ -7,23 +7,20 @@ pub use libsql;
 
 #[derive(Clone)]
 pub enum Database {
-    InMemory(libsql::Connection),
-    NotInMemory(Arc<libsql::Database>),
+    StaticConnection(libsql::Connection),
+    DynamicConnection(Arc<libsql::Database>),
 }
 
 impl Database {
     pub fn conn(&self) -> Result<libsql::Connection, crate::Error> {
         match self {
-            Database::InMemory(conn) => Ok(conn.clone()),
-            Database::NotInMemory(db) => db.connect().map_err(Into::into),
+            Database::StaticConnection(conn) => Ok(conn.clone()),
+            Database::DynamicConnection(db) => db.connect().map_err(Into::into),
         }
     }
 
     pub async fn sync(&self) -> Result<(), crate::Error> {
-        match self {
-            Database::InMemory(_) => Ok(()),
-            Database::NotInMemory(db) => db.sync().await.map_err(Into::into).map(|_| ()),
-        }
+        Ok(())
     }
 }
 
@@ -64,25 +61,24 @@ impl DatabaseBuilder {
             (Some(true), _, _) => {
                 let db = libsql::Builder::new_local(":memory:").build().await?;
                 let conn = db.connect()?;
-                Database::InMemory(conn)
+                Database::StaticConnection(conn)
             }
             (_, Some(path), None) => {
                 let db = libsql::Builder::new_local(path).build().await?;
-                Database::NotInMemory(Arc::new(db))
+                let conn = db.connect()?;
+                Database::StaticConnection(conn)
             }
             (_, None, Some((url, token))) => {
                 let db = libsql::Builder::new_remote(url, token).build().await?;
-                Database::NotInMemory(Arc::new(db))
+                Database::DynamicConnection(Arc::new(db))
             }
             (_, Some(path), Some((url, token))) => {
-                // https://github.com/tursodatabase/libsql/blob/d42e05a/libsql/examples/remote_sync.rs
-                // https://docs.rs/libsql/latest/libsql/struct.Builder.html#note
                 let db = libsql::Builder::new_remote_replica(path, url, token)
                     .read_your_writes(true)
                     .sync_interval(std::time::Duration::from_secs(300))
                     .build()
                     .await?;
-                Database::NotInMemory(Arc::new(db))
+                Database::DynamicConnection(Arc::new(db))
             }
             (_, None, None) => Err(crate::Error::InvalidDatabaseConfig(
                 "either '.memory()' or '.local()' or '.remote()' must be called".to_string(),
