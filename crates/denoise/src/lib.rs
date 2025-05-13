195fa947bf9166ce8d9e442d4b48ad6e56987e8a
yujonglee <yujonglee.dev@gmail.com>
2025-05-12 20:19:39 -0700

    more progress on denoise

--- crates/denoise/src/lib.rs
+++ crates/denoise/src/lib.rs
@@ -1,15 +1,33 @@
-use hypr_onnx::{load_model, ndarray, ort};
+mod error;
+
+pub use error::*;
+
+#[allow(unused)]
+use hypr_onnx::{
+    load_model, ndarray as nd,
+    ort::{session::Session, value::Tensor},
+};
+
+#[allow(unused)]
+use realfft::{num_complex::Complex, ComplexToReal, RealFftPlanner, RealToComplex};
 
 const MODEL_1_BYTES: &[u8] = include_bytes!("../data/model_1.onnx");
 const MODEL_2_BYTES: &[u8] = include_bytes!("../data/model_2.onnx");
 
+#[allow(unused)]
+const BLOCK_LEN: usize = 512;
+
+#[allow(unused)]
+const BLOCK_SHIFT: usize = 128;
+
+#[allow(unused)]
 pub struct DTLN {
-    model_1: ort::session::Session,
-    model_2: ort::session::Session,
+    model_1: Session,
+    model_2: Session,
 }
 
 impl DTLN {
-    pub fn new() -> Result<Self, ort::Error> {
+    pub fn new() -> Result<Self, crate::Error> {
         Ok(Self {
             model_1: load_model(&MODEL_1_BYTES)?,
             model_2: load_model(&MODEL_2_BYTES)?,
@@ -17,8 +35,8 @@ impl DTLN {
     }
 
     // https://github.com/breizhn/DTLN-aec/blob/main/run_aec.py
-    pub fn process(&self, _input: &[f32]) -> Result<Vec<f32>, ort::Error> {
-        Ok(vec![])
+    pub fn process(&self, input: &[f32]) -> Result<Vec<f32>, crate::Error> {
+        Ok(vec![0.0; input.len()])
     }
 }
 
