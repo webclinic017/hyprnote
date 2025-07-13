3b69e61abdaea3be33f2b4ff831fba43be304815
Yujong Lee <yujonglee.dev@gmail.com>
2025-07-12 21:19:50 -0700

    admin: chores

--- apps/admin/scripts/init.sh
+++ apps/admin/scripts/init.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 echo "Running database migrations..."
-npx drizzle-kit migrate
+drizzle-kit migrate
 
 echo "Starting application..."
-if command -v pnpm > /dev/null; then
-    exec pnpm start
-else
-    exec npm start
-fi
+exec pnpm start
