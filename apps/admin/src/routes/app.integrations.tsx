630512a111cd9a4cbb67bd9b1e6a91ae26c99273
Yujong Lee <yujonglee.dev@gmail.com>
2025-07-16 20:50:04 -0700

    admin: no personal integrations yet

--- apps/admin/src/routes/app.integrations.tsx
+++ apps/admin/src/routes/app.integrations.tsx
@@ -3,6 +3,7 @@ import {
   Badge,
   Box,
   Button,
+  Card,
   Center,
   Group,
   LoadingOverlay,
@@ -89,7 +90,9 @@ function Component() {
           </Tabs.Tab>
         </Tabs.List>
         <Tabs.Panel value="personal" className="mt-6">
-          <SettingsSection type="personal" />
+          <Card withBorder p="lg" radius="md">
+            There's no personal-level integrations yet
+          </Card>
         </Tabs.Panel>
         <Tabs.Panel value="organization" className="mt-6">
           <SettingsSection type="organization" />
