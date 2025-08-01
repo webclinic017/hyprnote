63dc20ad1603630e9534cbf8d846aae4176730d7
Yujong Lee <yujonglee.dev@gmail.com>
2025-08-01 10:13:39 -0700

    cli wip

--- owhisper/src/commands/readme.rs
+++ owhisper/src/commands/readme.rs
@@ -2,6 +2,6 @@
 pub struct ReadmeArgs {}
 
 pub async fn handle_readme(_args: ReadmeArgs) -> anyhow::Result<()> {
-    let _ = open::that("https://github.com/fastrepl/hyprnote");
+    let _ = open::that("https://github.com/fastrepl/hyprnote/blob/main/owhisper/README.md");
     Ok(())
 }
